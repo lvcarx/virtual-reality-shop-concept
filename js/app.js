f878aa918b259633ed7b57670f7e1486ffc2b3aa
Luca Reichmann <luca.reichmann@reichmann-it.de>
2019-12-29 12:18:24 +0100

    implementation of background changing

--- js/app.js
+++ js/app.js
@@ -1,3 +1,14 @@
 function goBack() {
     window.history.back();
 }
+
+// change background image
+
+const nextBtn = document.getElementById('previousBtn');
+nextBtn.addEventListener('click', changeBackground, false);
+
+function changeBackground() {
+
+    console.log('test');
+
+}
